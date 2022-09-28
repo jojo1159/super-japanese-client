78876f5f0c145c27921f44870ab2ee04c6498a97
nguyenhoangminh-rightsvn <103923179+nguyenhoangminh-rightsvn@users.noreply.github.com>
2022-09-28 20:05:22 +0700

    config vuestic

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { createVuestic } from "vuestic-ui";
+import "vuestic-ui/styles/essential.css";
+import "vuestic-ui/styles/grid.css";
+import "vuestic-ui/styles/reset.css";
+import "vuestic-ui/styles/typography.css";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(createVuestic()).mount("#app");
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,21 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <va-alert border="top" class="mb-4">
+      You read this important alert message.
+    </va-alert>
+    <va-alert color="success" border="right" class="mb-4">
+      You successfully read this important alert message.
+    </va-alert>
+    <va-alert color="info" border="bottom" class="mb-4">
+      This alert needs your attention, but it's not super important.
+    </va-alert>
+    <va-alert color="warning" border="left" class="mb-4">
+      Something looks weird.
+    </va-alert>
+    <va-alert color="gray" border="top" border-color="danger" class="mb-4">
+      Change a few things up and try submitting again.
+    </va-alert>
   </div>
 </template>
 
